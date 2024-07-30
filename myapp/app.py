d278b2ec91b056cc33789c6d38a07758c48a630f
oleksii.savchenko <alexey.savchenko.home@gmail.com>
2024-07-30 19:48:52 +0300

    Added validation call

---
+++ myapp/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from transformers import pipeline
+
+app = Flask(__name__)
+
+@app.route('/validate', methods = ['POST'])
+def process_request():
+    pipe = pipeline("text-classification", model="eliasalbouzidi/distilroberta-nsfw-text-classifier")
+
+    json = request.json
+
+    prompt = json.get('prompt')
+
+    if prompt != '':
+        validationResult = pipe(prompt)
+        validationLabel = validationResult[0]['label']
+        return jsonify(
+            result=validationLabel
+        )
+    else:
+        return jsonify(
+            result='invalid_input'
+        )
+
+if __name__ == '__main__':
+    app.run(debug=True)
